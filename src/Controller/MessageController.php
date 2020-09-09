85bbf3c456a6478d85b4b12e2c7f352d12c3c52a
cpalmer <cpalmer@friendmts.com>
2020-09-09 11:39:17 +0100

    Add Message Entity/Form/migration/Repo.

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -12,12 +12,29 @@ class MessageController extends AbstractController
 {
     public function send(): Response
     {
+        // Create the form
         $form = $this->createFormBuilder()
                      ->add('task', TextType::class)
                      ->add('dueDate', DateType::class)
                      ->add('save', SubmitType::class, ['label' => 'Create Task'])
                      ->getForm();
 
+        // on submit Validate the data, send the job
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        // render the form
         return $this->render('base.html.twig', [
             'form' => $form->createView(),
         ]);
---
+++ src/Entity/Message.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=140)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $phone_number;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $full_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(string $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->full_name;
+    }
+
+    public function setFullName(string $full_name): self
+    {
+        $this->full_name = $full_name;
+
+        return $this;
+    }
+}
